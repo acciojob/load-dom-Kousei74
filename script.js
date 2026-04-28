f6c8baa6c34389b1821559b6c49d03085bd90604
Suvam Poddar <98863065+Kousei74@users.noreply.github.com>
2026-04-28 12:20:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    let p = document.createElement("p");
+    p.textContent = "DOM load success";
+
+    document.body.appendChild(p);
+});
